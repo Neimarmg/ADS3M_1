d45be24522edc4316a8480c576e978b8dc2ed13d
Rafael Jeffman <rafael.jeffman@gmail.com>
2014-02-27 11:21:15 -0300

    Implementacao de lista encadeada com Lista e Nodo.

--- ListasEncadeadas/src/com/senac/estruturas/Nodo.java
+++ ListasEncadeadas/src/com/senac/estruturas/Nodo.java
@@ -13,13 +13,13 @@ public class Nodo {
 		this.next = null;
 	}
 	
-	private Nodo(int valor)
+	public Nodo(int valor)
 	{
 		this.dado = valor;
 		this.next = null;
 	}
 	
-	private int getData()
+	public int getData()
 	{
 		return dado;
 	}
@@ -29,48 +29,13 @@ public class Nodo {
 		this.dado = i;
 	}
 	
-	private Nodo getNext()
+	public Nodo getNext()
 	{
 		return next;
 	}
-	private void setNext(Nodo next)
+	public void setNext(Nodo next)
 	{
 		this.next = next;
-	}
-	
-	private static void insertBefore(Nodo novo, Nodo next)
-	{
-		novo.setNext(next);
-	}
-	
-	private static void insertAfter(Nodo anterior, Nodo novo)
-	{
-		novo.setNext(anterior.getNext());
-		anterior.setNext(novo);
-	}
-	
-	private static void print(Nodo head)
-	{
-		Nodo nodo = head;
-		do {
-			out.println(nodo.getData());
-			nodo = nodo.getNext();
-		} while (nodo != null); 
-	}
-
-	public static void main(String[] args)
-	{
-		Nodo head = new Nodo();
-		head.setData(2);
-		
-		Nodo novo = new Nodo(3);
-		insertAfter(head, novo);
-		
-		novo = new Nodo(1);
-		insertBefore(novo, head);
-		head = novo;
-		
-		print(head);
 	}
 
 }
