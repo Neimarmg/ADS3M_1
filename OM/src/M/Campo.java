52a7c99d6613183ee3fed1966c255fd6c4844f9e
neimar <neimarmg@hotmail.com>
2015-06-10 20:55:06 -0300

    2015 05 10 20:00

--- OM/src/M/Campo.java
+++ OM/src/M/Campo.java
@@ -1,65 +1,40 @@
 package M;
-
 import V.View;
-
 public class Campo {
 	
 	private static int i = 0 ,j = 0;
-
-	private static int linha = getLinha(); 
-	private static int coluna = getLinha();
-	public static String [][]mapa = new String[1][10];
+	public static String [][]mapa;
 	
-	
-	public static int getColuna() {
-		return coluna;
+	public Campo(int linha,int coluna) {
+		mapa = new String[linha][coluna];
 	}
-	
-	
-	public static void setColuna(int coluna) {
-		Campo.coluna = coluna;
-	}
-	
-	
-	public static void setLinha(int linha) {
-		Campo.linha = linha;
-	}
-	
-	
-	public static int getLinha() {
-		return linha;
-	}
-	
-	
-	public static void ml(){
-		if (j < getLinha()){
-			mapa[i][j] = ""+j;
+		
+	private static void imprime(boolean imprimir){
+		if(imprimir == true){
 			View.msg(" "+mapa[i][j]);
-			j++;
-			ml();
 		}
 	}
 	
+	private static void gerenciaExecucao(boolean gerar, boolean imprimir){
+		if(gerar == true){
+			mapa[i][j]= " "+j;
+			imprime(imprimir);
+		}else{
+			imprime(imprimir);
+		}
+	}
 	
-	
-	/**
-	 * Cria campo 
-	 * @throws Exception
-	 */
-	public static void mapa(boolean imprimir) throws Exception {
+	public static void executaMapa(boolean gerar, boolean imprimir) throws Exception {
 		try {
-			for (i = 0; i < linha; i++) {	
-				//j=0;
-				ml();
-				//for (j = 0; j < coluna; j++) {
-					//mapa[i][j] = "  ";
-					//View.msg(j+mapa[i][j]);
-				//}
+			for (i = 0; i < mapa.length; i++) {
+				for (j = 0; j < mapa.length; j++) {
+					gerenciaExecucao(gerar, imprimir);
+				}
 				View.msg("\n");
 			}
+						
 		} catch (ArrayIndexOutOfBoundsException e) {
 			View.msgb("Não foi possível criar o mapa!");
 		}
 	}
-
 }
--- OM/src/app/start.java
+++ OM/src/app/start.java
@@ -4,10 +4,7 @@ import M.Campo;
 
 public class start {
 	public static void main(String[] args)throws Exception {
-		Campo.setLinha(10);
-		Campo.setColuna(10);
-		Campo.ml();
-		//Campo.mapa(true);  
+		new Campo(15, 15);
+		Campo.executaMapa(true, true); 
 	}
-
 }
