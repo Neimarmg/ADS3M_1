554d1cc8533211b7d11b44a1621b14fc602e3db9
Neimar <neimarmg@hotmail.com>
2015-06-22 00:29:10 -0300

    2015 22 06

--- OM/src/app/start.java
+++ OM/src/app/start.java
@@ -6,7 +6,7 @@ import M.tamanho;
 
 public class start {
 	public static void main(String[] args)throws Exception {
-		new pagina(20, 20);
+		new pagina(25, 25);
 		tamanho.setAlturaLagura(5, 3);
 		pagina.executaFolha(true, false); 		
 		Escreve.marcaPontos(true, true);
