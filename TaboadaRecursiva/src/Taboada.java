8f10b200ecd30a42447a7cf5725c4b0267849d7e
Neimar <neimarmg@hotmail.com>
2015-10-04 17:44:16 -0300

    Nova varsão

--- TaboadaRecursiva/src/Taboada.java
+++ TaboadaRecursiva/src/Taboada.java
@@ -1,31 +1,76 @@
 
 public class Taboada {
-	static int numerador = 0, denomidor = 0;	
+	static int numerador = 0, denominador = 0;	
+	static String resultado = "";
 	
-	public static String calcula(){
-		denomidor++;
-		return denomidor +" * " +numerador +" = " +(denomidor * numerador);
+	
+	public static void setResult(String result, boolean limparResult) {
+		if (limparResult == false){
+			resultado += result;
+		}else{
+			resultado = "";
+		}
 	}
+
+
+	public static void efetuaCalculos(String operador){
+
+		switch (operador) {	
+		case "*":
+			setResult("" +(denominador * numerador), false);
+			break;
+		
+		case "/":
+			if (numerador > 0){
+				setResult("" +(denominador / numerador), false);
+			}else{
+				setResult("0", false);
+			}
+			break;
+
+		case "-":
+			setResult("" +(denominador - numerador), false);			
+			break;
+
+		case "+":
+			setResult("" +(denominador + numerador), false);			
+			break;
+		}
+		
+	}
+		
 	
-	public static void imprime(){
-		if (denomidor < 10) {
-			 System.out.println(calcula());
-			 imprime();
+	public static void executaCalculo(String operador){
+		denominador++;
+		efetuaCalculos(operador);
+	}
+
+	
+	public static void imprimeResultados(String operador){
+		if (denominador < 10) {	
+			setResult(denominador +" " +operador +" " +numerador +" = ", false);	
+			executaCalculo(operador);
+			System.out.println(resultado);
+			setResult("", true);
+			imprimeResultados(operador);
 		}else{
 			numerador++;
-			denomidor = 0;
+			denominador = 0;
 		}			
 	}
 	
-	public static void taboada(int tabAte){
+	
+	public static void carregaParametros(int tabAte, String operador){
 		if (numerador < tabAte) {
 			System.out.println("\nTaboada do: "+numerador); 
-			imprime();
-			taboada(tabAte);
+			imprimeResultados(operador);
+			carregaParametros(tabAte, operador);
 		}		
 	}
 	
+	
 	public static void main(String[] args) {
-		Taboada.taboada(10);
+		System.out.println("\nExecução de taboada\n");
+		Taboada.carregaParametros(20, "+");
 	}
 }
