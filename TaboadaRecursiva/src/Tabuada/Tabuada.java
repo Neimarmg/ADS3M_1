673984bfb338f73ec4c03596bfa00043a563ab51
Neimar <neimarmg@hotmail.com>
2016-01-23 12:09:39 -0200

    2016 01 26 12: 03

---
+++ TaboadaRecursiva/src/Tabuada/Tabuada.java
@@ -0,0 +1,82 @@
+package Tabuada;
+
+public class Tabuada {
+	static float numerador = 0, denominador = 0;	
+	static String valorResultado = "";
+	
+	
+	public static void setResult(String result, boolean limparResult) {
+		if (limparResult == false){
+			valorResultado += result;
+		}else{
+			valorResultado = "";
+		}
+	}
+
+
+	public static void efetuaCalculos(String operador){
+
+		switch (operador) {	
+			case "*":
+				setResult("" +(denominador * numerador), false);
+				break;
+			
+			case "/":
+				if (numerador > 0){
+					setResult("" +(denominador / numerador), false);
+				}else{
+					setResult("0", false);
+				}
+				break;
+	
+			case "-":
+				setResult("" +(denominador - numerador), false);			
+				break;
+	
+			case "+":
+				setResult("" +(denominador + numerador), false);			
+				break;	
+		default:
+				setResult("Operação inválida!",false);
+			
+		}
+		
+	}
+		
+	
+	public static void executaCalculo(String operador){
+		denominador++;
+		efetuaCalculos(operador);
+	}
+
+	
+	public static void imprimeResultados(String Operacao){
+		if (denominador < 10) {	
+			setResult(denominador +" " +Operacao +" " +numerador +" = ", false);	
+			executaCalculo(Operacao);
+			System.out.println(valorResultado);
+			setResult("", true);
+			imprimeResultados(Operacao);
+		}else{
+			numerador++;
+			denominador = 0;
+		}			
+	}
+	
+	
+	public static void carregaParametros(int calcularTabudaAte, String operador){
+		if (numerador <= calcularTabudaAte) {
+			View.msgcr("\nTabuada do: "+numerador); 
+			imprimeResultados(operador);
+			carregaParametros(calcularTabudaAte, operador);
+		}		
+	}
+	
+	
+	public static void main(String[] args) {
+		View.msgcr("\nT a b u a d a\n");
+		Tabuada.carregaParametros(
+				View.digitaNumero("\nCalcular tabuada até")
+				,View.digitaString("\nOperação"));
+	}
+}
