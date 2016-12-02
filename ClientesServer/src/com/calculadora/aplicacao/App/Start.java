ba9be858380ddbbc87d7f0ca0722ff1de92bda7e
Neimar <neimarmg@hotmail.com>
2016-12-01 23:40:12 -0200

    2016 12 1 2215

--- ClientesServer/src/com/calculadora/aplicacao/App/Start.java
+++ ClientesServer/src/com/calculadora/aplicacao/App/Start.java
@@ -1,7 +1,7 @@
 package com.calculadora.aplicacao.App;
 
 import java.rmi.RemoteException;
-
+import javax.security.sasl.SaslException;
 import javax.xml.rpc.ServiceException;
 
 import com.calculadora.aplicacao.C.parametrizador;
@@ -9,7 +9,7 @@ import com.calculadora.aplicacao.V.View;
 
 public class Start {
 	
-	public static void main(String args[]) throws ServiceException, RemoteException {
+	public static void main(String args[]) throws SaslException, RemoteException, ServiceException {
 		View.msgl();
 		parametrizador.criaOperacao();
 		parametrizador.print();
